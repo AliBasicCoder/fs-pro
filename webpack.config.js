0165f1a4377e24a866691d0d7afb706d6f02c02e
alianmad <aliahmedreda34@gmail.com>
2020-03-21 01:01:58 +0200

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  output: {
+    filename: "fs-pro.min.js",
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "commonjs"
+  },
+  target: "node",
+  node: {
+    fs: "empty",
+    path: "empty"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
+};
