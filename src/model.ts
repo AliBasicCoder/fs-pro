fcc68d898cadc647cfc3919d6f859ecb26a60746
alianmad <aliahmedreda34@gmail.com>
2020-03-20 23:13:21 +0200

    adding model basic functionality

--- src/model.ts
+++ src/model.ts
@@ -1,19 +1,50 @@
-interface modelFileObj {
-  isFile: true;
-  ext: string;
+import { writeFileSync, mkdirSync } from "fs";
+import { join } from "path";
+
+interface modelObjBase<T extends string> {
+  type: T;
 }
 
+type modelFileObj = modelObjBase<"file"> & { ext: string };
+type modelDirObj = modelObjBase<"dir"> & { fileType: modelFileObj };
+
 interface modelData {
-  [key: string]: modelFileObj | modelData;
+  // @ts-ignore
+  __any?: modelFileObj;
+  // @ts-ignore
+  __any_dir?: modelDirObj;
+  [key: string]: modelDirObj | modelFileObj | modelData;
+}
+
+const isModelFileObj = (obj: any): obj is modelFileObj => obj.type === "file";
+const isModelDirObj = (obj: any): obj is modelDirObj => obj.type === "dir";
+
+function createAt(data: modelData, path: string) {
+  mkdirSync(path);
+  for (const key in data) {
+    const element = data[key];
+    if (key === "__any" || key === "__any_dir") continue;
+    if (isModelFileObj(element))
+      writeFileSync(join(path, `${key}${element.ext}`), "");
+    else if (isModelDirObj(element)) mkdirSync(join(path, key));
+    else {
+      createAt(element, join(path, key));
+    }
+  }
 }
 
 export class Model {
   public static File(ext: string): modelFileObj {
-    return {
-      isFile: true,
-      ext
-    };
+    return { type: "file", ext };
+  }
+
+  public static Dir(fileType: modelFileObj): modelDirObj {
+    return { type: "dir", fileType };
   }
 
   constructor(public data: modelData) {}
+
+  createAt(path: string) {
+    createAt(this.data, path);
+  }
 }
