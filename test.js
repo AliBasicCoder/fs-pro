407ffb59732a3e56d01bee4b99cbd62647bbf9d0
alianmad <aliahmedreda34@gmail.com>
2020-03-22 15:54:23 +0200

    removing test

--- test.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-const { Dir } = require(".");
-
-const nd = new Dir(__dirname, "node_modules");
-
-const time = Date.now();
-nd.delete();
-console.log(Date.now() - time);
