db81da3557a8c0226b42f864de91c422ea99485f
alianmad <aliahmedreda34@gmail.com>
2020-07-23 09:29:07 +0200

    ..

--- src/model.ts
+++ src/model.ts
@@ -34,7 +34,8 @@ export class Model {
   }
   /**
    * create a Model object
-   * ```js
+   * @param data the object that represents the schema (model) your dir will be
+   * @example
    * const model = new Model({
    *   src: {
    *      file1: Model.File(".js")
@@ -43,14 +44,13 @@ export class Model {
    * })
    * modal.createAt(__dirname);
    * // ...
-   * ```
-   * @param data the object that represents the schema (model) your dir will be
    */
   constructor(public data: modelData) {}
 
   /**
    * create a structure of File and Dir objects
-   * ```js
+   * @param path the path to use
+   * @example
    * import { Model } from "fs-pro";
    *
    * const modelBase = {
@@ -60,8 +60,6 @@ export class Model {
    * const structure = (new Model(modelBase)).structure<typeof modelBase>(__dirname);
    *
    * structure.node_modules instanceof Dir === "true"
-   * ```
-   * @param path the path to use
    */
   structure<T extends modelData>(...paths: string[]): sw<T> {
     const path = join(...paths);
@@ -71,10 +69,11 @@ export class Model {
   }
 
   /**
-   * create a structure of File and Dir objects
-   * and calls .create in all of theme
-      * example:
-   * ```js
+   * create a structure of File and Dir objects and calls .create in all of theme
+   * @see https://fs-pro-docs.herokuapp.com/classes/_file_.file.html#create
+   * @see https://fs-pro-docs.herokuapp.com/classes/_dir_.dir.html#create
+   * @param path the path to create files at
+   * @example
    * import { Model } from "fs-pro";
    *
    * const modelBase = {
@@ -84,11 +83,6 @@ export class Model {
    * const structure = (new Model(modelBase)).createAt<typeof modelBase>(__dirname);
    *
    * structure.node_modules instanceof Dir === "true"
-   * ```
-
-   * @see https://fs-pro-docs.herokuapp.com/classes/_file_.file.html#create
-   * @see https://fs-pro-docs.herokuapp.com/classes/_dir_.dir.html#create
-   * @param path the path to create files at
    */
   createAt<T extends modelData>(
     path: string,
@@ -98,20 +92,17 @@ export class Model {
     return createAt<T>(this.data, path, options);
   }
   /**
-   * makes sure that a given directory matches a given
-   * model
-   * NOTE: throws an error if the directory is not valid see .valid()
-   * ```js
-   * model.validate(dirToValidate);
-   * ```
+   * makes sure that a given directory matches a given model
    * @param data the model data you want to validate passed on
    * @param path the path of the file you want to validate
    * @param options functions to call on errors
+   * @NOTE throws an error if the directory is not valid see .valid()
+   * @example
+   * model.validate(dirToValidate);
    */
   validate(path: string, options?: Partial<validateOptions>) {
     validate(path, this.data, options);
   }
-
   /** the same as .validate() but don't throw an error and returns a boolean */
   valid(path: string) {
     try {
