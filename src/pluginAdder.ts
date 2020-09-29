c258b2af43fc018be63a69df9fcfb222b32bd0fb
alianmad <aliahmedreda34@gmail.com>
2020-09-29 04:59:21 +0200

    improving plugins

--- src/pluginAdder.ts
+++ src/pluginAdder.ts
@@ -1,10 +1,44 @@
 import * as index from "./index";
 import { Plugin } from "./types";
 
+const native_methods = {
+  File: {
+    static: Object.keys(index.File),
+    inst: Object.keys(index.File.prototype),
+  },
+  Dir: {
+    static: Object.keys(index.Dir),
+    inst: Object.keys(index.Dir.prototype),
+  },
+};
+
+const track: {
+  pluginName: string;
+  methodName: string;
+  className: string;
+  isStatic: boolean;
+}[] = [];
+
+function checkIfOverwrites(element: Plugin["plugin"][0], pluginName: string) {
+  for (let i = 0; i < track.length; i++) {
+    const { className, methodName, isStatic } = track[i];
+    if (
+      element.className === className &&
+      element.methodName === methodName &&
+      element.isStatic === isStatic
+    )
+      throw new Error(
+        `Plugin ${pluginName} tries to overwrite ${className}.${methodName}() added by plugin: ${pluginName}`
+      );
+  }
+  return -1;
+}
+
 /**
  * adds plugins to the core library
  * plugins is a way to add methods to the core class like File, Dir, etc...
  * @param pluginWrapper the plugin
+ * @param allowPluginOverwrite if `true` will allow provided plugin to overwrite any method added by any plugin
  * @example
  * addPlugin({
  *   name: "xml",
@@ -19,19 +53,38 @@ import { Plugin } from "./types";
  *     }]
  *  });
  */
-export function addPlugin(pluginWrapper: Plugin) {
+export function addPlugin(
+  pluginWrapper: Plugin,
+  allowPluginOverwrite: boolean = false
+) {
   const { requires, plugin } = pluginWrapper;
   if (requires) {
-    requires.forEach(addPlugin);
+    requires.forEach((rq) => addPlugin(rq));
   }
   for (let i = 0; i < plugin.length; i++) {
     const element = plugin[i];
     if (element.isStatic) {
+      if (native_methods[element.className].static.includes(element.methodName))
+        throw new Error(
+          `Plugin "${pluginWrapper.name}" tries to overwrite native methods`
+        );
+      if (!allowPluginOverwrite) checkIfOverwrites(element, pluginWrapper.name);
       // @ts-ignore
       index[element.className][element.methodName] = element.func;
     } else {
+      if (native_methods[element.className].inst.includes(element.methodName))
+        throw new Error(
+          `Plugin "${pluginWrapper.name}" tries to overwrite native methods`
+        );
+      if (!allowPluginOverwrite) checkIfOverwrites(element, pluginWrapper.name);
       // @ts-ignore
       index[element.className].prototype[element.methodName] = element.func;
     }
+    track.push({
+      pluginName: pluginWrapper.name,
+      className: element.className,
+      methodName: element.methodName,
+      isStatic: element.isStatic,
+    });
   }
 }
--- test/node/plugin.ts
+++ test/node/plugin.ts
@@ -1,7 +1,7 @@
 import * as assert from "assert";
 import { File, addPlugin } from "../../src/index";
 
-it("addPlugin", (done) => {
+it("addPlugin normal plugin", (done) => {
   addPlugin({
     name: "xml",
     plugin: [
@@ -35,5 +35,59 @@ it("addPlugin", (done) => {
   // @ts-ignore
   assert.equal(File.st(), "hello there");
   file.delete();
+  done();
+});
+
+it("errors when overwriting native methods", (done) => {
+  assert.throws(() => {
+    addPlugin({
+      name: "test",
+      plugin: [
+        {
+          className: "File",
+          methodName: "write",
+          isStatic: false,
+          func() {},
+        },
+      ],
+    });
+  });
+  done();
+});
+
+it("errors when overwriting methods added by other plugins", (done) => {
+  assert.throws(() => {
+    addPlugin({
+      name: "test_2",
+      plugin: [
+        {
+          className: "File",
+          methodName: "xml",
+          isStatic: false,
+          func() {},
+        },
+      ],
+    });
+  });
+  done();
+});
+
+it("allow overwriting methods added by other plugins when set to true", (done) => {
+  assert.doesNotThrow(() => {
+    addPlugin(
+      {
+        name: "test_2",
+        plugin: [
+          {
+            className: "File",
+            methodName: "xml",
+            isStatic: false,
+            func() {},
+          },
+        ],
+      },
+      true
+    );
+  });
   done();
 });
