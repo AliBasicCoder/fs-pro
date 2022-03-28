11472bc446d4f3921bf4d8b04b503d5a9ca8218b
AliBasicCoder <aliahmedreda34@gmail.com>
2022-03-28 21:33:32 +0200

    ..

--- test/deno/deno_runner.ts
+++ test/deno/deno_runner.ts
@@ -4,44 +4,31 @@ import "./file.test.ts";
 import "./plugin.test.ts";
 import "./shape.test.ts";
 import { setImports, tests } from "./imports.ts";
+import { existsSync, statSync } from "https://deno.land/std@0.131.0/node/fs.ts";
+import { join, parse } from "https://deno.land/std@0.131.0/path/mod.ts";
+import {
+  assertEquals,
+  assert,
+  assertThrows,
+} from "https://deno.land/std@0.131.0/testing/asserts.ts";
 
-async function load_deno() {
-  const [fs, assert, path] = await Promise.all([
-    import("https://deno.land/std@0.131.0/node/fs.ts"),
-    import("https://deno.land/std@0.131.0/testing/asserts.ts"),
-    import("https://deno.land/std@0.131.0/path/mod.ts"),
-  ]);
+const tmp_dir = parse(Deno.makeTempDirSync()).dir;
+
+function load_deno() {
   setImports({
-    assertEquals: assert.assertEquals,
-    assert: assert.assert,
-    assertThrows: assert.assertThrows,
-    join: path.join,
-    parse: path.parse,
-    existsSync: fs.existsSync,
-    statSync: fs.statSync,
+    assertEquals,
+    assert,
+    assertThrows,
+    join,
+    parse,
+    existsSync,
+    statSync,
     makeTempFileSync: Deno.makeTempFileSync,
     makeTempDirSync: Deno.makeTempDirSync,
     readTextFileSync: Deno.readTextFileSync,
     writeTextFileSync: Deno.writeTextFileSync,
     resources: Deno.resources,
-    tempDir: () => {
-      // copied from https://deno.land/x/temp_dir@v1.0.0/mod.ts
-      const getDirectory = () => {
-        if (Deno.build.os === "windows") {
-          return (
-            Deno.env.get("TMP") ||
-            Deno.env.get("TEMP") ||
-            Deno.env.get("USERPROFILE") ||
-            Deno.env.get("SystemRoot") ||
-            ""
-          );
-        }
-
-        return Deno.env.get("TMPDIR") || "/tmp";
-      };
-
-      return Deno.realPathSync(getDirectory());
-    },
+    tempDir: () => tmp_dir,
   });
   for (const test_ of tests) {
     Deno.test({
